937df1d43f29f98e127ec7f1f1fd21c883a2949d
vo1dcod3 <145624896+vo1dcod3@users.noreply.github.com>
2023-12-28 10:30:29 -0300

    EntregaFinalv1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const servicios = [
-        { id: 1, nombre: "Netflix", precio: 9.90 },
-        { id: 2, nombre: "CrunchyRoll", precio: 8.99 },
-        { id: 3, nombre: "Disney+", precio: 7.99 },
-        { id: 4, nombre: "HBO Max", precio: 6.99 },
-        { id: 5, nombre: "Apple TV+", precio: 5.99 }
-    ];
-
+document.addEventListener('DOMContentLoaded', async () => {
     let carrito = [];
 
-    // Recuperar carrito desde localStorage al cargar la página
-    const carritoGuardado = localStorage.getItem('carrito');
-    if (carritoGuardado) {
-        carrito = JSON.parse(carritoGuardado);
-        actualizarContadorCarrito();
-    }
+    // Función para cargar servicios 
+    const cargarServiciosDesdeJSON = async () => {
+        try {
+            const response = await fetch('servicios.json');
+            if (!response.ok) {
+                throw new Error(`Error al cargar servicios: ${response.status}`);
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(error.message);
+            throw error; 
+        }
+    };
 
-    function mostrarServicios() {
+    // Función para mostrar la suma total de servicios seleccionados
+    const mostrarSumaTotal = () => {
+        const sumaTotal = carrito.reduce((total, servicio) => total + servicio.precio, 0);
+        const sumaTotalElemento = document.getElementById("suma-total");
+
+        if (sumaTotalElemento) {
+            sumaTotalElemento.textContent = `La suma total de los servicios seleccionados es: $${sumaTotal.toFixed(2)}`;
+        }
+    };
+
+    // Función para actualizar el contador del carrito
+    const actualizarContadorCarrito = () => {
+        document.getElementById("contador-carrito").textContent = carrito.length;
+    };
+
+    // Función para mostrar los servicios en el DOM
+    const mostrarServicios = async () => {
         const listaServicios = document.getElementById("lista-servicios");
 
         if (!listaServicios) {
@@ -24,48 +39,42 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        listaServicios.innerHTML = ""; // Limpiar la lista antes de mostrar los servicios
+        listaServicios.innerHTML = ""; 
 
-        servicios.forEach(servicio => {
-            const listItem = document.createElement("li");
-            listItem.className = "list-group-item d-flex justify-content-between align-items-center";
-            listItem.innerHTML = `
-                ${servicio.nombre} - Precio: $${servicio.precio}
-                <div>
-                    <button class="btn btn-primary btn-sm" onclick="agregarAlCarrito(${servicio.id})">Agregar al Carrito</button>
-                    <button class="btn btn-danger btn-sm ms-2" onclick="eliminarDelCarrito(${servicio.id})">Eliminar</button>
-                </div>
-            `;
-            listaServicios.appendChild(listItem);
-        });
+        try {
+            const servicios = await cargarServiciosDesdeJSON();
 
-        // Botón para mostrar la suma total
-        const botonSumaTotal = document.createElement("button");
-        botonSumaTotal.className = "btn btn-success mt-3";
-        botonSumaTotal.textContent = "Mostrar Suma Total";
-        botonSumaTotal.onclick = mostrarSumaTotal;
-        listaServicios.appendChild(botonSumaTotal);
+            servicios.forEach(({ nombre, precio, id }) => {
+                const listItem = document.createElement("li");
+                listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+                listItem.innerHTML = `
+                    ${nombre} - Precio: $${precio}
+                    <div>
+                        <button class="btn btn-primary btn-sm" onclick="agregarAlCarrito(${id})">Agregar al Carrito</button>
+                        <button class="btn btn-danger btn-sm ms-2" onclick="eliminarDelCarrito(${id})">Eliminar</button>
+                    </div>
+                `;
+                listaServicios.appendChild(listItem);
+            });
 
-        // Elemento para mostrar la suma total
-        const sumaTotalElemento = document.createElement("p");
-        sumaTotalElemento.id = "suma-total";
-        listaServicios.appendChild(sumaTotalElemento);
-    }
+            // Botón para mostrar la suma total
+            const botonSumaTotal = document.createElement("button");
+            botonSumaTotal.className = "btn btn-success mt-3";
+            botonSumaTotal.textContent = "Mostrar Suma Total";
+            botonSumaTotal.onclick = mostrarSumaTotal;
+            listaServicios.appendChild(botonSumaTotal);
 
-    function mostrarSumaTotal() {
-        const sumaTotal = carrito.reduce((total, servicio) => total + servicio.precio, 0);
-        const sumaTotalElemento = document.getElementById("suma-total");
-
-        if (sumaTotalElemento) {
-            sumaTotalElemento.textContent = `La suma total de los servicios seleccionados es: $${sumaTotal.toFixed(2)}`;
+            // Elemento para mostrar la suma total
+            const sumaTotalElemento = document.createElement("p");
+            sumaTotalElemento.id = "suma-total";
+            listaServicios.appendChild(sumaTotalElemento);
+        } catch (error) {
+            console.error(error.message);
         }
-    }
+    };
 
-    function actualizarContadorCarrito() {
-        document.getElementById("contador-carrito").textContent = carrito.length;
-    }
-
-    window.agregarAlCarrito = function (servicioId) {
+    // Función para agregar un servicio al carrito
+    window.agregarAlCarrito = async (servicioId) => {
         const servicioExistente = carrito.find(servicio => servicio.id === servicioId);
 
         if (servicioExistente) {
@@ -73,17 +82,24 @@ document.addEventListener('DOMContentLoaded', function () {
             return;
         }
 
-        const servicioSeleccionado = servicios.find(servicio => servicio.id === servicioId);
-        if (servicioSeleccionado) {
-            carrito.push(servicioSeleccionado);
-            actualizarContadorCarrito();
-            mostrarServicios();
-            // Guardar carrito en localStorage
-            localStorage.setItem('carrito', JSON.stringify(carrito));
+        try {
+            const servicios = await cargarServiciosDesdeJSON();
+            const servicioSeleccionado = servicios.find(servicio => servicio.id === servicioId);
+
+            if (servicioSeleccionado) {
+                carrito.push(servicioSeleccionado);
+                actualizarContadorCarrito();
+                mostrarServicios();
+                // Guardar carrito en localStorage
+                localStorage.setItem('carrito', JSON.stringify(carrito));
+            }
+        } catch (error) {
+            console.error(error.message);
         }
     };
 
-    window.eliminarDelCarrito = function (servicioId) {
+    // Función para eliminar un servicio del carrito
+    window.eliminarDelCarrito = (servicioId) => {
         carrito = carrito.filter(servicio => servicio.id !== servicioId);
         actualizarContadorCarrito();
         mostrarServicios();
@@ -91,5 +107,39 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('carrito', JSON.stringify(carrito));
     };
 
-    mostrarServicios();
+    // Modal de suscripción
+    const btnSuscribir = document.getElementById("btnSuscribir");
+    const modalSuscripcion = document.getElementById("modalSuscripcion");
+    const cerrarModal = document.getElementById("cerrarModal");
+    const formularioSuscripcion = document.getElementById("formularioSuscripcion");
+
+    if (btnSuscribir && modalSuscripcion && cerrarModal && formularioSuscripcion) {
+        btnSuscribir.addEventListener("click", () => {
+            modalSuscripcion.style.display = "block";
+        });
+
+        cerrarModal.addEventListener("click", () => {
+            modalSuscripcion.style.display = "none";
+        });
+
+        window.addEventListener("click", (event) => {
+            if (event.target === modalSuscripcion) {
+                modalSuscripcion.style.display = "none";
+            }
+        });
+
+        formularioSuscripcion.addEventListener("submit", async (event) => {
+            event.preventDefault();
+
+            if (validarSuscripcion()) {
+                modalSuscripcion.style.display = "none";
+                alert("Gracias por suscribirte.");
+            } else {
+                alert("Por favor, ingresa un correo electrónico válido.");
+            }
+        });
+    }
+
+ 
+    await mostrarServicios(); 
 });
